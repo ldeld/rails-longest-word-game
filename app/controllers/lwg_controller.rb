c07b86db7e86177d118afe89f973df1ce9c4b650
Lorenzo Del Castillo <l.delcastillo.97@gmail.com>
2017-11-20 16:55:45 +0100

    changed controller to use hidden inputs instead of class variables

--- app/controllers/lwg_controller.rb
+++ app/controllers/lwg_controller.rb
@@ -4,22 +4,29 @@ require 'json'
 class LwgController < ApplicationController
 # GRID = (0...15).map { (65 + rand(26)).chr }
   def game
-    @@start_time = Time.now
-    @@grid = (0...15).map { (65 + rand(26)).chr }
-  end
-
-  helper_method :grid
-  def grid
-    @@grid
+    @start_time = Time.now
+    @grid = (0...15).map { (65 + rand(26)).chr }
   end
 
   def score
     end_time = Time.now
-    calculate_score(params[:attempt], (end_time - @@start_time).round(3))
+    total_time = (end_time - DateTime.parse(params[:start_time])).round(3)
+    calculate_score(params[:grid_string].split(""), params[:attempt], total_time)
+
+    session[:scores] = [] if session[:scores].nil?
+    scores = session[:scores]
+    scores << @results[:score]
+    session[:scores] = scores
+
+    @avg_score = avg_score(scores).round(2)
   end
 
-  def calculate_score(attempt, time) #(attempt, start_time, end_time)
-    if !word_in_grid?(attempt)
+  def avg_score(scores)
+    scores.inject{ |sum, element| sum + element }.to_f / scores.size
+  end
+
+  def calculate_score(grid, attempt, time) #(attempt, start_time, end_time)
+    if !word_in_grid?(attempt, grid)
       @results = { time: time, score: 0, message: "Your word uses a letter that is not in the grid" }
     elsif word_in_dictionary?(attempt)
       @results = { time: time, score: (attempt.length * 20 / time).round(2), message: "well done" }
@@ -28,11 +35,11 @@ class LwgController < ApplicationController
     end
   end
 
-  def word_in_grid?(attempt)
+  def word_in_grid?(attempt, grid)
     attempt.upcase!
     attempt_ar = attempt.chars.sort.uniq
     attempt_ar.each do |ltr|
-      return false if attempt.count(ltr) > @@grid.count(ltr)
+      return false if attempt.count(ltr) > grid.count(ltr)
     end
     return true
   end
